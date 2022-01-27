d7a414d3fedc34de1e116209fd5302945c9578bd
Dan13l4 <kdbaron426@gmail.com>
2022-01-27 10:54:34 -0500

    adding comments

--- src/cipher.js
+++ src/cipher.js
@@ -1,43 +1,69 @@
 const cipher = {
 
+  //Funcion del cifrador, aqui declaro el nombre de unas de las funciones dentro del objeto del cipher
   encode : function(offset, string){
+    //Este if funciona para que si en alguno de los parametros no hay nada, debe tirar un TypeError
     if (!offset || !string) {
       throw new TypeError();
     }
+    //Este if funciona para que si el parametro string no es un texto y el parametro offset no es un numero, tire un error de TypeError
     if (typeof(string) !== 'string' || typeof(offset) !== 'number' ) {
       throw new TypeError();
     }
+
+    //Aqui estoy declarando las funciones que usare mas adelante
     let arrayPhrase = [];
     let newWord;
+
+    //Aqui utilize un bucle for para empezar a crear el cifrador, lo primero que hize fue definir que i valdria cero y dependiendo de cuantas letras tenga la palabra a cifrar se ira descomponiendo letra a letra
+    //y cifrando con el charCodeAt y utilizando la formula
     for(let i = 0; i < string.length; i++){
       let numberAscii = string.charCodeAt(i);
       let equation = ((numberAscii - 65 + offset)% 26) + 65;
+
+    //Y desde aqui las letras ya cifradas se uniran en una sola array con push y con join se transforman en texto
       newWord = String.fromCharCode(equation);
       arrayPhrase.push(newWord);
       newWord = arrayPhrase.join("");
     }
+
+    //Y ya con return se finaliza toda la funcion y devuelve el valor de newWord
     return newWord;
   },
 
+  //Funcion del descifrador, este es la segunda funcion dentro del objeto
   decode : function(offset, string){
+    //Este if funciona para que si en alguno de los parametros no hay nada, debe tirar un TypeError
     if (!offset || !string) {
       throw new TypeError();
     }
+    //Este if funciona para que si el parametro string no es un texto y el parametro offset no es un numero, tire un error de TypeError
     if (typeof(string) !== 'string' || typeof(offset) !== 'number' ) {
       throw new TypeError();
     }
+
+    //Aqui estoy declarando las funciones que usare mas adelante
     let arrayPhrase = [];
     let newWord;
+
+    //Aqui utilize un bucle for para empezar a crear el descrifrador, lo primero que hize fue definir que i valdria cero y dependiendo de cuantas letras tenga la palabra a descifrar se ira descomponiendo letra a letra
+    //y descifrando con el charCodeAt y utilizando la formula, esta vez de manera negativa
     for(let i = 0; i < string.length; i++){
       let numberAscii = string.charCodeAt(i);
       let equation = ((numberAscii - 65 - offset)% 26) + 65;
+
+      //Este if lo que hace es que si el resultado del codigo ASCII es menor a 65 sumara nuevamente las 26 letras del abecedario para repetir el array desde el ultimo elemento
       if (equation < 65) {
         equation = equation + 26;
       }
+
+      //Y desde aqui las letras ya cifradas se uniran en una sola array con push y con join se transforman en texto
       newWord = String.fromCharCode(equation);
       arrayPhrase.push(newWord);
       newWord = arrayPhrase.join("");
     }
+
+    //Y ya con return se finaliza toda la funcion y devuelve el valor de newWord
     return newWord;
   }
 
--- src/index.js
+++ src/index.js
@@ -6,12 +6,20 @@ console.log(cipher);
    // window.alert("Bienvend@, porfavor lee con atencion las siguientes instrucciones.  \nCIFRADOR:   \n1. En mayusculas porfavor ingresa el texto que quiera cifrar, trata de no usar caracteres especiales como la coma, tilde o la letra ñ \n2. Escoge el numero de pasos que quieres que se corra la letra en el abecedario. \n3. Pulsa el boton -cifrar- para ver el resultado. \nDECIFRADOR: \n1.En mayusculas porfavor ingresa el texto que quiera Decifrar \n2. Escoge el numero de pasos que se usaron para cifrar la palabra \n3. Pulsa el boton -descifrar- para ver el resultado.");
 //});
 
+
+//Aqui empiezo la funcionalidad de los botones, usando un getElementById para usar el boton del cifrado y un escuchador de eventos, que hara que cuando
+//El usuario haga click empieze a correr el codigo
   document.getElementById("button").addEventListener("click", ()=>{
     let string = document.getElementById("input-original").value;
     let offset = document.getElementById("rango").value;
+    //Aqui declaro que el valor de string estara en el cuadro donde el usuario pone su texto y el offset el numero que ponga en la barra de rango para que el codigo del cifrador tenga sus parametros definidos
+    //Con el .value hago que coja solo el texto que hay en el cuadro del textarea y el numero que puso el usuario en el rango para poder hacer el cifrado
     offset = parseInt(offset);
+    //El parseInt lo que hace es transformar el valor del offset a tipo number para evitar un typeError
     let newWord = cipher.encode(offset, string);
+    //Con este let lo que hago es llamar la funcion de cifrado del objeto cipher, y toma los let de string y offset para definir los parametros que se usaran para cifrar
     document.getElementById("texto").innerHTML = newWord;
+    //Y por ultimo utilizo el innerHTML para sobreescribir el texto que se encuentra en el textarea del final y sacar el resultado
   });
 
   document.getElementById("buton").addEventListener("click", ()=>{
